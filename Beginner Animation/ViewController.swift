a83b3802f67a4aee6bcbaf0b0ffc6febeeb4b07b
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-10-17 23:35:08 +0530

    Basic Animation

--- Beginner Animation/ViewController.swift
+++ Beginner Animation/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var block: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let tap = UITapGestureRecognizer(target: self, action: #selector(blockTapped))
+        block.addGestureRecognizer(tap)
+        block.isUserInteractionEnabled = true
     }
 
+    @objc func blockTapped() {
+        UIView.animate(withDuration: 1.2) {
+            
+            let sizeMutliplier: CGFloat =  2.5
+//            let currentBlockFrame = self.block.frame
+//            self.block.frame.size = CGSize(width: currentBlockFrame.width * sizeMutliplier, height: currentBlockFrame.height * sizeMutliplier)
+            
+            let newWidth = self.block.frame.width * sizeMutliplier
+            let newHeight = self.block.frame.height * sizeMutliplier
+            
+            let newX = self.block.frame.origin.x - (newWidth - self.block.frame.size.width) / 2
+            let newY = self.block.frame.origin.y - (newHeight - self.block.frame.size.height) / 2
+            
+            self.block.frame = CGRect(x: newX, y: newY, width: newWidth, height: newHeight)
+        }
+    }
 
 }
 
